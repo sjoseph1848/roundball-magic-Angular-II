d38ef464ac3fe469a80bb4e25c8165a4c596a423
sjoseph1848 <sjoseph1848@gmail.com>
2020-08-24 18:45:48 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
+import { GamesComponent } from './pages/games/games.component';
+import { NewsComponent } from './pages/news/news.component';
+import { StatsComponent } from './pages/stats/stats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    GamesComponent,
+    NewsComponent,
+    StatsComponent
   ],
   imports: [
     BrowserModule,
